4c1933b6579761f1601dee120a9e9822e6aa0030
lizhengang9 <lizhengang9@jd.com>
2022-07-27 21:47:10 +0800

    fix: 新增大佬名字

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   budaId: 54511363, // 布大楼id，每月更新
   shortId: 54512382, // 驴大楼id，每月更新
   zhaiId: 44235537, // 转债楼id
-  highlightList: ['希夏邦驴', '东方无愧', '再碰中位emo', '波king', '鲁西有救了', '国足啊队长'], // 大佬们的名字会变成黄色
+  highlightList: ['希夏邦驴', '东方无愧', '再碰中位emo', '波king', '鲁西有救了', '国足啊队长', 'gechid'], // 大佬们的名字会变成黄色
 };
 
 exports = module.exports = config;
