8936a5db951356822320e9982730dcf140b1b922
lizhengang9 <lizhengang9@jd.com>
2022-11-11 20:53:31 +0800

    update

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  budaId: 55355678, // 布大楼id，每月更新
-  shortId: 55356214, // 驴大楼id，每月更新
+  budaId: 56212603, // 布大楼id，每月更新
+  shortId: 56207459, // 驴大楼id，每月更新
   zhaiId: 44235537, // 转债楼id
-  highlightList: ['希夏邦驴', '东方无愧', '再碰中位emo', '波king', '鲁西有救了', '国足啊队长', 'gechid'], // 大佬们的名字会变成黄色
+  highlightList: ['希夏邦驴', '再碰中位emo', '鲁西有救了', '国足啊队长', 'gechid', '陈筠璞小宝贝', '虎扑JR0713141282', '虎扑JR1258873610', '节奏的大师' ], // 大佬们的名字会变成黄色
 };
 
 exports = module.exports = config;
